6e2eb80e627a0563185d488bf9a02c7b1a6a7b43
GOLD <techas@tlen.pl>
2019-09-08 11:09:53 +0200

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\GOLD\\Desktop\\chromedriver75.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.booking.com");
+
+        System.out.println(driver.getTitle());
+        WebElement element = driver.findElement(By.className("sb-destination__input"));
+        System.out.println(element.getAttribute("placeholder"));
+
+    }
+}
